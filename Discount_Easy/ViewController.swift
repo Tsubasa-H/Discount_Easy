8ada2c355eb804c9ceaf4644105829216792b5c7
服部翼 <hattoritsubasa@hattoritsubasanomacbook-pro.local>
2019-04-11 22:19:29 +0900

    Initial Commit

---
+++ Discount_Easy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Discount_Easy
+//
+//  Created by 服部翼 on 2019/04/11.
+//  Copyright © 2019 服部翼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
